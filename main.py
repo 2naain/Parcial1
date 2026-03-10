50efa962ebeb821671bb365e819f5c97f6cf325e
2naain <jdlozano31@ucatolica.edu.co>
2026-03-10 14:55:36 -0500

    nose

--- main.py
+++ main.py
@@ -8,11 +8,6 @@ caballeros = []
 
 
 
-@app.post("/caballero")
-def create_caballero(c: Caballero):
-    caballeros.append(c)
-    return {"message": f"Caballero {c.name} creado"}
-
 @app.get("/showallcaballeros")
 def showallcaballeros():
     return [c.showcaballero() for c in caballeros]
