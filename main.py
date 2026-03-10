81692e6d28e8d540ef7caa65114ad0a1059639ed
2naain <jdlozano31@ucatolica.edu.co>
2026-03-10 14:54:05 -0500

    modelos y mein

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 from fastapi import FastAPI
+from models import Caballero
 
 app = FastAPI()
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+caballeros = []
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str | None = None):
-    return {"item_id": item_id, "q": q}
 
-    asdasdasdasd
+@app.post("/caballero")
+def create_caballero(c: Caballero):
+    caballeros.append(c)
+    return {"message": f"Caballero {c.name} creado"}
+
+@app.get("/showallcaballeros")
+def showallcaballeros():
+    return [c.showcaballero() for c in caballeros]
+
---
+++ models.py
@@ -0,0 +1,24 @@
+from enum import Enum, auto
+from pydantic import BaseModel
+
+class Material(Enum):
+    BRONCE = auto()
+    PLATA = auto()
+    ORO = auto()
+
+
+class Caballero(BaseModel):
+    id: int
+    name: str
+    material: Material
+    attack: int
+    constelation: str
+
+    def showcaballero(self):
+        return f"Caballero {self.name} con armadura {self.material.name}"
+
+    def fightcaballero(self):
+        return f"{self.name} ataca con poder {self.attack}"
+
+    def showconstelation(self):
+        return f"Constelación: {self.constelation}"
